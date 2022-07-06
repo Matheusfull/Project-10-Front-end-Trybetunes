09aed5ea84d84de435bbcc3110ea3f0b1feb1079
Matheus <fpmitmatheus@yahoo.com>
2022-07-06 01:23:30 -0300

    requisito 1, vamos com força total

---
+++ src/pages/Favorites.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Favorite extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-favorites">
+        <h1> Favorite</h1>
+      </div>
+    );
+  }
+}
+
+export default Favorite;
